b7edea4b0e90541bb93a968dddda46dc68fa0606
euichanhwang <pine6971@naver.com>
2023-08-22 13:37:54 +0900

    add:

---
+++ bigstone/basic/stl.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define X first
+#define Y second
+typedef pair<int, int> pii;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+/**
+ * 컨테이너: C++에서 제공하는 자료구조
+ *
+ * 시퀀스 컨테이너(sequence container)는 데이터를 단순히 저장해 놓는 자료구조를 뜻하며
+ * array, vector, deque, forward_list, list 가 있다.
+ *
+ * 연관 컨테이너(associative container)는 자료가 저장됨에 따라 자동정렬되는 자료구조를 말한다.
+ * 중복키가 가능한 것은 이름에 multi가 붙는다.
+ * set, map, multiset, multimap이 있다.
+ *
+ * 정렬되지 않은 연관 컨테이너(unordered associative container)는 자료가 저장됨에 따라 자동정렬이 되지 않는
+ * 자료구조를 말한다. unordered_set, unordered_map, unordered_multiset, unordered_multimap이 있다.
+ *
+ * 컨테이너 어뎁터(container adapter)는 시퀀스 컨테이너를 이용해 만든 자료구조를 뜻하며
+ * stack, queue는 deque로, priority_queue는 vector를 이용해 힙 자료구조로 만든다.
+ */
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+}
